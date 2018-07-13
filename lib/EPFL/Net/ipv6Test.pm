8c4603adca6b56ea3e6af3bc3e10a85ea0f6e53a
William Belle <william.belle@gmail.com>
2018-07-13 15:45:44 +0200

    init module

---
+++ lib/EPFL/Net/ipv6Test.pm
@@ -0,0 +1,117 @@
+package EPFL::Net::ipv6Test;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+EPFL::Net::ipv6Test - The great new EPFL::Net::ipv6Test!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use EPFL::Net::ipv6Test;
+
+    my $foo = EPFL::Net::ipv6Test->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+William Belle, C<< <william.belle at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-epfl-net-ipv6test at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=EPFL-Net-ipv6Test>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc EPFL::Net::ipv6Test
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=EPFL-Net-ipv6Test>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/EPFL-Net-ipv6Test>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/EPFL-Net-ipv6Test>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/EPFL-Net-ipv6Test>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2018 William Belle.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    L<http://www.apache.org/licenses/LICENSE-2.0>
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+=cut
+
+1; # End of EPFL::Net::ipv6Test
---
+++ t/00-load.t
@@ -0,0 +1,12 @@
+use 5.006;
+use strict;
+use warnings;
+use Test::More;
+
+plan tests => 1;
+
+BEGIN {
+    use_ok( 'EPFL::Net::ipv6Test' ) || print "Bail out!\n";
+}
+
+diag( "Testing EPFL::Net::ipv6Test $EPFL::Net::ipv6Test::VERSION, Perl $], $^X" );
